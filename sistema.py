063ff3554d8c6a22f9ea0bfd48808f6e87b7346b
Aderson Longo <adersonlongo@gmail.com>
2026-06-17 20:33:59 -0300

    Adicionado codigo fatos

---
+++ sistema.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from datetime import datetime
+
+
+def obter_info_sistema() -> dict:
+    return {
+        "sistema": sys.platform,
+        "diretorio": os.getcwd,
+        "data": datetime.now().isoformat(),
+    }
